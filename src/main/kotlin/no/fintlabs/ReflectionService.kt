de3677be473840f59f91c9b47d4830a644b7597e
Hknots <hknots99@gmail.com>
2024-09-06 20:45:21 +0200

    add reflectionService that fills MetadataCache

---
+++ src/main/kotlin/no/fintlabs/ReflectionService.kt
@@ -0,0 +1,49 @@
+package no.fintlabs
+
+import jakarta.annotation.PostConstruct
+import no.fint.model.FintModelObject
+import org.reflections.Reflections
+import org.springframework.stereotype.Service
+
+@Service
+class ReflectionService(
+    val metadataCache: MetadataCache,
+) {
+
+    @PostConstruct
+    fun fillCache() {
+        Reflections("no.fint.model")
+            .getSubTypesOf(FintModelObject::class.java)
+            .forEach { clazz ->
+                println(clazz.name)
+                val fintModelObject = newInstanceOfFintModelObject(clazz)
+                val relationsMetadata = fintModelObject.relations.map { relation ->
+                    FintRelationMetadata(
+                        relation.name.lowercase(),
+                        relation.multiplicity,
+                        relation.packageName
+                    )
+                }
+
+                metadataCache.add(
+                    Metadata(
+                        clazz.packageName.split(".")[3],
+                        clazz.packageName.split(".").getOrNull(4),
+                        clazz.simpleName.lowercase(),
+                        fintModelObject.identifikators.keys,
+                        fintModelObject.isWriteable,
+                        relationsMetadata
+                    )
+                )
+            }
+    }
+
+    fun newInstanceOfFintModelObject(clazz: Class<out FintModelObject>): FintModelObject {
+        try {
+            return clazz.getDeclaredConstructor().newInstance()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+}
