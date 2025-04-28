a7178ad5a990e4bb9276b826868e4d27efa00e25
Hknots <hknots99@gmail.com>
2025-04-28 11:11:16 +0200

    feat: integrate MetamodelProperties into MetadataCache for enhanced component name formatting

--- src/main/kotlin/no/fintlabs/metamodel/metadata/MetadataCache.kt
+++ src/main/kotlin/no/fintlabs/metamodel/metadata/MetadataCache.kt
@@ -1,12 +1,17 @@
 package no.fintlabs.metamodel.metadata
 
+import no.fintlabs.metamodel.metadata.config.MetamodelProperties
 import no.fintlabs.metamodel.metadata.model.Resource
 import org.springframework.stereotype.Service
 
 @Service
-class MetadataCache {
+class MetadataCache(
+    private val metamodelProperties: MetamodelProperties
+) {
 
     private val cache: MutableMap<String, MutableSet<Resource>> = mutableMapOf()
+
+    fun getComponents() = cache.keys
 
     fun getResources(domainName: String, packageName: String): Collection<Resource> =
         cache.getOrDefault(formatComponentName(domainName, packageName), mutableSetOf())
@@ -20,6 +25,7 @@ class MetadataCache {
     fun addResource(domainName: String, packageName: String, resource: Resource) =
         getOrSetDefault(domainName, packageName).add(resource)
 
-    private fun formatComponentName(domainName: String, packageName: String) = "$domainName.$packageName"
+    private fun formatComponentName(domainName: String, packageName: String) =
+        "$domainName${metamodelProperties.format}$packageName"
 
 }
