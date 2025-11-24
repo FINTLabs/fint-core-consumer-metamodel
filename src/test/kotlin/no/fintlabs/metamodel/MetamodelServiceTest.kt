a97153998fde230f3babc47ee310609ccf2e9355
Hknots <hknots99@gmail.com>
2025-11-24 21:15:51 +0100

    test: add tests for FINT version 3.19 component list and validation

--- src/test/kotlin/no/fintlabs/metamodel/MetamodelServiceTest.kt
+++ src/test/kotlin/no/fintlabs/metamodel/MetamodelServiceTest.kt
@@ -11,6 +11,35 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest(classes = [ApplicationTest::class])
 class MetamodelServiceTest {
 
+    companion object {
+        private val COMPONENTS_V3_19 = listOf(
+            "arkiv.samferdsel",
+            "utdanning.larling",
+            "utdanning.vurdering",
+            "administrasjon.kodeverk",
+            "utdanning.kodeverk",
+            "utdanning.elev",
+            "okonomi.regnskap",
+            "administrasjon.personal",
+            "ressurs.tilgang",
+            "arkiv.noark",
+            "utdanning.ot",
+            "ressurs.kodeverk",
+            "utdanning.timeplan",
+            "arkiv.kodeverk",
+            "utdanning.utdanningsprogram",
+            "arkiv.kulturminnevern",
+            "ressurs.eiendel",
+            "okonomi.faktura",
+            "personvern.kodeverk",
+            "personvern.samtykke",
+            "arkiv.personal",
+            "administrasjon.organisasjon",
+            "okonomi.kodeverk",
+            "administrasjon.fullmakt"
+        )
+    }
+
     @Autowired
     lateinit var service: MetamodelService
 
@@ -22,14 +51,29 @@ class MetamodelServiceTest {
         fun `by component+resource works`() {
             val res = service.getResource("utdanning.vurdering", "elevfravar")
             assertNotNull(res)
-            assertEquals("Elevfravar", res!!.name)
+            assertEquals("elevfravar", res!!.name)
         }
 
         @Test
         fun `by domain+pkg+resource works`() {
             val res = service.getResource("utdanning", "vurdering", "elevfravar")
             assertNotNull(res)
-            assertEquals("Elevfravar", res!!.name)
+            assertEquals("elevfravar", res!!.name)
+        }
+
+    }
+
+    @Nested
+    inner class GetComponent {
+
+        @Test
+        fun `FINT version 3_19 has expected number of components`() =
+            assertEquals(COMPONENTS_V3_19.size, service.getComponents().size)
+
+        @Test
+        fun `FINT version 3_19 components match expected names`() {
+            val componentNames = service.getComponents().map { it.name }.toSet()
+            assertEquals(COMPONENTS_V3_19.toSet(), componentNames)
         }
 
     }
